24ad977ec06f4e9ea07abc85e1f0e931fc7e0b8e
pranavg01 <pranav.g.gupta@gmail.com>
2016-12-11 12:25:09 -0500

    Create scale.js

---
+++ scale.js
@@ -0,0 +1,10 @@
+var scaleObject = prompt("What is the diameter of your scaled sun?");
+var scaleFactor = 1391400000/scaleObject;
+
+//object constructor
+var planet = new Object();
+this.diameter = this.diameter;
+this.distance = this.distance;
+this.scaledDiameter = function(){
+return this.diameter * scale;
+}
