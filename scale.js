c65269f5326b7b55121c205ed3f4f8b600588349
pranavg01 <pranav.g.gupta@gmail.com>
2016-12-11 13:00:38 -0500

    Update scale.js

--- scale.js
+++ scale.js
@@ -6,5 +6,18 @@ var planet = new Object();
 this.diameter = this.diameter;
 this.distance = this.distance;
 this.scaledDiameter = function(){
-return this.diameter * scale;
+return this.diameter*scaleFactor;
 }
+this.scaledDistance = function(){
+ return this.distance*scaleFactor; 
+}
+
+var mercury = new planet();
+mercury.diameter = 4879000;
+mercury.distance = 57910000000;
+mercury.scaledDiameter();
+mercury.scaledDistance();
+
+var venus = new planet();
+venus.diameter = 12104000;
+venus.distance = 
