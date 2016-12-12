7c2b119b7958e1463a63ff60c26e139fcbcc693a
pranavg01 <pranav.g.gupta@gmail.com>
2016-12-11 21:24:48 -0500

    Update scale.js

--- scale.js
+++ scale.js
@@ -4,60 +4,48 @@ var scaleObject = prompt("What is the diameter of your scaled sun?");
 var scaleFactor = 1391400000/scaleObject;
 
 //planet constructor
-var planet = new Object();
-this.diameter = this.diameter;
-this.distance = this.distance;
+
+function planet(diameter, distance){
+this.diameter = diameter;
+this.distance = distance;
 this.scaledDiameter = function(){
-return this.diameter*scaleFactor;
+return diameter*scaleFactor;
 };
 this.scaledDistance = function(){
- return this.distance*scaleFactor; 
+ return distance*scaleFactor; 
 };
+}
 
-var mercury = new planet();
-mercury.diameter = 4879000;
-mercury.distance = 57910000000;
+var mercury = new planet(4879000, 57910000000);
 mercury.scaledDiameter();
 mercury.scaledDistance();
 
-var venus = new planet();
-venus.diameter = 12104000;
-venus.distance = 108200000000;
+var venus = new planet(12104000, 108200000000);
 venus.scaledDiameter();
 venus.scaledDistance();
 
-var earth = new planet();
+var earth = new planet(12742000, 149600000000);
 earth.diameter = 12742000;
 earth.distance = 149600000000;
 earth.scaleDiameter();
 earth.scaleDistance();
 
-var mars = new planet();
-mars.diameter = 6779000;
-mars.distance = 227900000;
+var mars = new planet(6779000, 227900000);
 mars.scaleDiameter();
 mars.scaleDistance();
 
-var jupiter = new planet();
-jupiter.diameter = 139822000;
-jupiter.distance = 778500000000;
+var jupiter = new planet(139822000, 778500000000);
 jupiter.scaleDiameter();
 jupiter.scaleDistance();
 
-var saturn = new planet();
-saturn.diameter = 116464000;
-saturn.distance = 1429000000000;
+var saturn = new planet(116464000, 1429000000000);
 saturn.scaleDiameter();
 saturn.scaleDistance();
 
-var uranus = new planet();
-uranus.diameter = 50724000;
-uranus.distance = 2871000000000;
+var uranus = new planet(50724000, 2871000000000);
 uranus.scaleDiameter();
 uranus.scaleDistance();
 
-var neptune = new planet();
-neptune.diameter = 50724000;
-neptune.distance = 2871000000000;
+var neptune = new planet(50724000, 2871000000000);
 neptune.scaleDiameter();
 neptune.scaleDistance();
