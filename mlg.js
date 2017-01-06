d95f8241662a0546fbe2969e32c468e67b6a3063
pranavg01 <pranav.g.gupta@gmail.com>
2017-01-05 22:54:39 -0500

    Update mlg.js

--- mlg.js
+++ mlg.js
@@ -1,6 +1,4 @@
-function JOE(){
-
-var x = Math.floor((Math.random() * 10) + 1);
-
-document.GetElementById("box").innerHTML = x;
+function myFunction() {
+    var x = Math.floor((Math.random() * 10) + 1);
+    document.getElementById("demo").innerHTML = x;
 }
